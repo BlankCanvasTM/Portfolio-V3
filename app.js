0cd7a2e1f63ee7fc81c6b440086762b7e71737ca
BlankCanvasTM <BlankCanvasTM@users.noreply.github.com>
2023-01-18 22:44:01 +0000

    Added functionality for fading in items

---
+++ app.js
@@ -0,0 +1,30 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('shown');
+        }else{
+            entry.target.classList.remove('shown');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+const fadingElements = document.querySelectorAll('.fadeIn')
+fadingElements.forEach((el) => observer2.observe(el));
